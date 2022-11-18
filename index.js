db649920d744738adaccbdb4914ff72babc90326
shipwoli <khalwalesaviour@gmail.com>
2022-11-18 09:57:10 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,44 +1,30 @@
-function shout (aWord) {
-  return aWord.toUpperCase( );
+
+
+function shout(string) {
+  return string.toUpperCase();
 }
 
-function whisper (aWord) {
-  return aWord.toLowerCase( );
+function whisper(string) {
+    return string.toLowerCase();
+}    
+
+function logShout(string) {
+console.log(string.toUpperCase());
 }
 
-function logShout (aWord) {
-  console.log(aWord.toUpperCase( ));
+function logWhisper(string) {
+    console.log(string.toLowerCase());
 }
 
-function logWhisper (aWord) {
-  console.log(aWord.toLowerCase( ));
-}
-
-
-// describe('sayHiToGrandma(string)', function() {
-//   it('returns "I can\'t hear you!" if `string` is lowercase', function() {
-//     expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
-//   })
-
-//   it('returns "YES INDEED!" if `string` is uppercase', function() {
-//     expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
-//   })
-
-//   it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
-//     expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
-//   })
-// })
-function sayHiToGrandma (aWord) {
-  var cantUnswer = "I can't hear you!";
-  var yesUnswer = "YES INDEED!";
-  var lovUnswer = "I love you, too.";
-  if (aWord.toLowerCase(aWord) === aWord) {
-    return cantUnswer;
-  }
-  else if (aWord.toUpperCase(aWord) === aWord) {
-    return yesUnswer;
-  }
-  else if ("I love you, Grandma." === aWord) {
-    return lovUnswer
+function sayHiToHeadphonedRoommate(string) {
+   
+    if ("hello" === string) {
+      return "I can't hear you!";
+    }
+    else if ("HELLO" === string) {
+      return "YES INDEED!";
+    }
+    else if ("Let's have dinner together!" === string) {
+      return "I would love to!";
   }
 }
